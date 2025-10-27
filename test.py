2482ef6618ec3d71609925713607bf7b752e9cbc
tomerbraunstain <tomer@jit.io>
2025-10-27 09:10:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ fetch_zip_files('http://example.com/archive.zip')
 
 def load_multiple_yamls(*file_paths):
     combined_data = {} 
-    for file_path in file_paths:
+    for file_path in file_paths: 
         with open(file_path, 'r') as yaml_file:
             file_data = yaml.unsafe_load(yaml_file) 
             combined_data.update(file_data)
