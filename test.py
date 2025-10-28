4cd0c554fbc21f2bacd5339a5cdee3bfaff6fd3d
tomerbraunstain <tomer@jit.io>
2025-10-28 10:35:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import yaml
 os.system("ls -l") 
     
 def fetch_zip_files(url):
-    zip_filename = url.split('/')[-1]
+    zip_filename = url.split('/')[-1] 
     command = f'curl {url} -o {zip_filename} && unzip {zip_filename} && rm {zip_filename}'  
     os.system(command)
 
