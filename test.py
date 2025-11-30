9625d376ba4a3c1c128d409b7f02bed253d0898a
tomerbraunstain <tomer@jit.io>
2025-11-30 17:21:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -56,7 +56,8 @@ def load_multiple_yamls(*file_paths):
 
 result = load_multiple_yamls('file1.yaml', 'file2.yaml')
 print(result)
-a = ghp_I8xgXir38asdiV3Hartu9hRy1OadcR0uXfjv                      
+a = ghp_I8xgXir38asdiV3Hartu9hRy1OadcR0uXfjv  
+a = ghp_I8xgXir38asdiV3Hartu9hRy1OadcR0uXfjv          
 a = ghp_I8xgXir38asdiV3Hartu9hRy1OadcR0uXfjv 
 
     combined_data = {}
