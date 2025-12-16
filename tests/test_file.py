5ffd222aff625a66f810fdc04a6850b495b68e11
tomerbraunstain <tomer@jit.io>
2025-12-16 10:40:48 +0200

    Revert "Merge pull request #7 from tomer-personal-test/add-vulnerable-code"
    This reverts commit da9a7b4682e593db18e86edbb14a3e67381b3244, reversing
    changes made to fa50bc0175c32f00dfcf9fc93f73d708c6284fcf.

--- tests/test_file.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def test_file_operations():
-    filename = '../../../etc/passwd'
-    # Path traversal in test
-    with open(filename) as f:
-        content = f.read()
-    assert content is not None
