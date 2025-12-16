1c515fdb337d77415adf405ab156c33dd089220d
tomerbraunstain <tomer@jit.io>
2025-12-16 10:21:27 +0200

    Add middleware, config, and library files with vulnerabilities

---
+++ config/jwt_config.py
@@ -0,0 +1,7 @@
+# JWT configuration
+JWT_SECRET_KEY = 'super_secret_jwt_key_123'
+JWT_ALGORITHM = 'HS256'
+JWT_REFRESH_SECRET = 'refresh_token_secret_456'
+
+# No expiration time configured
+JWT_ACCESS_TOKEN_EXPIRES = False
